2d93f83111d78d3ee96ce9361433c445939af8fc
daltonfontes <fontes.dalton@hotmail.com>
2023-01-23 22:56:29 -0300

    feat : add get cookies

--- index.js
+++ index.js
@@ -6,22 +6,20 @@ import puppeteer from 'puppeteer';
     });
     const page = await browser.newPage();
 
-    // emulate galaxy 8
+    // emulate Iphone 14
     await page.setUserAgent('Mozilla/5.0 (iPhone; CPU iPhone OS 12_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.1 Mobile/15E148 Safari/604.1');
-    await page.setViewport({ width: 414, height: 896, deviceScaleFactor: 2 });
+    await page.setViewport({ width: 390, height: 844, deviceScaleFactor: 3 });
 
     //goto to page
     await page.goto('https://lolesports.com/');
 
     //Handle cookies
-    const cookies = await page.cookies()
-    if (cookies.length === 0) {
+    const getCookies = await page.cookies()
+    if (getCookies.length === 0) {
         console.log('No cookies found')
     } else {
-        console.log(cookies)
+        console.log(getCookies)
     }
-
-    console.log(cookies[0].name);
     //get element
     //close browser
     await browser.close();
