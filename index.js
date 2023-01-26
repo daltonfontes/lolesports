e317071fc19017b54b78a4e66d8cb6113337cc78
daltonfontes <fontes.dalton@hotmail.com>
2023-01-25 23:28:26 -0300

    feat : get vods links

--- index.js
+++ index.js
@@ -1,26 +1,34 @@
 import puppeteer from 'puppeteer';
 
-(async () => {
-    const browser = await puppeteer.launch({
+const run = async () => {
 
-    });
+    // Launch browser
+    const browser = await puppeteer.launch();
     const page = await browser.newPage();
 
-    // emulate Iphone 14
+    // Emulate Iphone 14
     await page.setUserAgent('Mozilla/5.0 (iPhone; CPU iPhone OS 12_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.1 Mobile/15E148 Safari/604.1');
     await page.setViewport({ width: 390, height: 844, deviceScaleFactor: 3 });
 
-    //goto to page
-    await page.goto('https://lolesports.com/');
+    // Goto to page
+    await page.goto('https://lolesports.com/vods/cblol-brazil/cblol_2023_split_1');
 
-    //Handle cookies
-    const getCookies = await page.cookies()
-    if (getCookies.length === 0) {
+    // Handle cookies
+    const cookies = await page.cookies()
+    if (cookies.length === 0) {
         console.log('No cookies found')
-    } else {
-        console.log(getCookies)
     }
-    //get element
-    //close browser
+
+    // Get Vods events
+    const text = await page.$eval('.list', el => el.outerHTML);
+
+    // Get game list
+    const games = text.split('<div class="VodsMatch match">');
+
+    // Get game links
+    const links = games.map(game => game.split('<a class="wrapper "href=">'));
+
+    // Close browser
     await browser.close();
-})();
+};
+run();
